73f137e0a8b0b8ff04d25e1b8b67e848780dd34d
Eliel Silva <elielbarros.silva@gmail.com>
2021-08-31 21:47:14 -0300

    aulas 16 a 19 adicionadas

--- JavaScript-Refresh/Aula12/play.js
+++ JavaScript-Refresh/Aula12/play.js
@@ -35,7 +35,7 @@ console.log(summarizeUser2(name, age, hasHobbies));
 
 console.log(summarizeUser3(name, age, hasHobbies));
 
-console.log(add(1,2));
+console.log(add(1, 2));
 
 console.log(addOne(1));
 
@@ -54,7 +54,6 @@ const person = {
         console.log('Hi, I am ' + this.name);
     }
 };
-
 person.greet();
 
 const person1 = {
@@ -64,5 +63,72 @@ const person1 = {
         console.log('Hi, I am ' + this.name);
     }
 };
-
 person1.greet();
+
+// --------------------------- Aula 16 Arrays & Array Methods ---------------------------
+const hobbies = ['Sports', 'Cooking'];
+for (let hobby of hobbies) {
+    console.log(hobby)
+}
+
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+console.log(hobbies);
+
+// --------------------------- Aula 17 Arrays, Objects & Reference Types ---------------------------
+hobbies.push('Programming');
+console.log(hobbies);
+// Dar push de um novo 'objeto' em uma const array não significa que vamos 'modificar' ela.
+
+// --------------------------- Aula 18 Understanding Spread & Rest Operators ---------------------------
+// Criar uma nova array com todos os valores antigos mais o novo valor
+const copiedArray = hobbies.slice(); // copia uma array
+const copiedArray2 = [...hobbies]; // copia uma array tambem
+console.log(copiedArray);
+console.log(copiedArray2);
+
+// Vc pode copiar uma pessoa da mesma forma
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
+// Temos uma questão, nao um problema. Temos uma função que recebe tres argumentos..
+// Mas eu nao quero receber os argumentos assim, pois se eu quiser acrescentar mais um, teria que acrescentar tambem a função
+// A solução vem apos esse exemplo
+const toArray = (arg1, arg2, arg3) => {
+    return [arg1, arg2, arg3];
+}
+
+console.log(toArray(1, 2, 3, 4));
+
+// To resolve this question do:
+// Merge multiple arguments into an array
+const toArray2 = (...args) => {
+    return args;
+}
+
+console.log(toArray2(1, 2, 3, 4, 5, 6));
+
+// --------------------------- Aula 19 Destructuring ---------------------------
+// FORMA ESTRUTURADA
+const printName = (person) => {
+    console.log(person.name);
+}
+printName(person);
+
+// FORMA DESESTRUTURADA
+const printName2 = ({ name }) => {
+    console.log(name);
+}
+printName2(person);
+
+
+const person3 = {
+    namePerson: 'Eliel', agePerson: '25'
+}
+const { namePerson, agePerson } = person3;
+console.log(namePerson, agePerson);
+
+const hobbies3 = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies3;
+console.log(hobby1, hobby2);
+
+// --------------------------- Aula 20 Async & Promises ---------------------------
