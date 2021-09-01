926be07d0451982320f64c64a59db05d3514102a
Eliel Silva <elielbarros.silva@gmail.com>
2021-08-31 22:11:26 -0300

    aula 20 adicionada

--- JavaScript-Refresh/play.js
+++ JavaScript-Refresh/play.js
@@ -132,3 +132,39 @@ const [hobby1, hobby2] = hobbies3;
 console.log(hobby1, hobby2);
 
 // --------------------------- Aula 20 Async & Promises ---------------------------
+
+// FIRST APPROACH
+// const fetchData = callback => {
+//     setTimeout(() => {
+//         callback('Done!');
+//     }, 1500);
+// }
+
+// setTimeout(() => {
+//     console.log('Timer is done!');
+//     fetchData(text => {
+//         console.log(text);
+//     })
+// }, 2000);
+
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });
+
+    return promise;
+}
+
+setTimeout(() => {
+    console.log('Timer is done!');
+    fetchData()
+    .then( text => {
+        console.log(text);
+        return fetchData();
+    })
+    .then( text2 => {
+        console.log(text2);
+    });
+}, 2000);
